6bae700bb4132fa520ef82a791c2b197d1de5d35
Riku Nummi <rikunummi9@gmail.com>
2026-01-08 17:44:00 +0200

    13.16 done

---
+++ controllers/authors.js
@@ -0,0 +1,22 @@
+const router = require('express').Router()
+const { Blog } = require('../models')
+const { fn, col } = require('sequelize') 
+
+router.get('/', async (req, res, next) => {
+  try {
+    const authors = await Blog.findAll({
+      attributes: [
+        'author',
+        [fn('COUNT', col('id')), 'articles'],
+        [fn('SUM', col('likes')), 'likes'],
+      ],
+      group: ['author'],
+    })
+
+    res.json(authors)
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
