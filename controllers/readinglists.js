5c4240c88a612bfacfc15974ce980e33a0be698f
Riku Nummi <rikunummi9@gmail.com>
2026-01-09 17:58:19 +0200

    13.22 done

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -14,4 +14,23 @@ router.post('/', tokenExtractor, async (req, res, next) => {
   }
 })
 
+router.put('/:id', tokenExtractor, async (req, res) => {
+  try {
+    const id = Number(req.params.id)
+    const entry = await ReadingList.findByPk(id)
+    if (!entry) return res.status(404).end()
+
+    if (entry.userId !== req.decodedToken.id) {
+      return res.status(403).json({ error: 'not allowed' })
+    }
+    entry.read = req.body.read
+    await entry.save()
+
+    return res.json(entry)
+
+  } catch (error) {
+    next(error)
+  }
+})
+
 module.exports = router
